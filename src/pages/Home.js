3737eb27c6d5c2bfb8b592305d262bd6d4cedfb7
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-05-29 01:07:05 +0200

    Update home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import {HiHome} from "react-icons/hi";
+import {Button} from "flowbite-react";
+import {BarChart, DeliveryTruck, Package} from "fluent-emoji";
 
 class Home extends React.Component {
+
     render() {
         return (
-            <div className="">
+            <div>
                 <section className="bg-white dark:bg-gray-900">
-                    <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+                    <div className="grid border-b border-gray-200 dark:border-gray-700 py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                         <div className="place-self-center mr-auto lg:col-span-7">
                             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">EcoCircuits</h1>
                             <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Une
@@ -34,6 +38,89 @@ class Home extends React.Component {
                         </div>
                     </div>
                 </section>
+                <div className="dark:bg-gray-900 dark:text-white">
+                    <div className="md:flex md:space-x-3 md:space-y-0 space-y-2 py-8 px-4 mx-auto max-w-screen-xl">
+                        <div className="bg-white md:w-1/3 p-8 dark:bg-gray-800 dark:rounded">
+                            <div className="flex pb-4 items-center space-x-5">
+                                <div>
+                                    <BarChart className="w-12"/>
+                                </div>
+                                <div className="text-lg font-medium">
+                                    Visualisez et gérez vos stocks en temps réel !
+                                </div>
+                            </div>
+                            <div className="border-t text-gray-500 dark:text-gray-400 pt-4 border-gray-200 dark:border-gray-700">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
+                                libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+                                imperdiet. Duis sagittis ipsum. Praesent mauris.
+                            </div>
+                            <div className="pt-4">
+                                <Button>
+                                    En savoir plus
+                                    <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"></path>
+                                    </svg>
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="bg-white md:border-l md:dark:border-0 border-gray-200 md:w-1/3 p-8 dark:bg-gray-800 dark:rounded">
+                            <div className="flex pb-4 items-center space-x-5">
+                                <div>
+                                    <Package className="w-16"/>
+                                </div>
+                                <div className="text-lg font-medium">
+                                    Gérez vos commandes et vos tournées en quelques clics !
+                                </div>
+                            </div>
+                            <div className="border-t text-gray-500 dark:text-gray-400 pt-4 border-gray-200 dark:border-gray-700">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
+                                libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+                                imperdiet. Duis sagittis ipsum. Praesent mauris.
+                            </div>
+                            <div className="pt-4">
+                                <Button>
+                                    En savoir plus
+                                    <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"></path>
+                                    </svg>
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="bg-white md:border-l md:dark:border-0 border-gray-200 md:w-1/3 p-8 dark:bg-gray-800 dark:rounded">
+                            <div className="flex pb-4 items-center space-x-5">
+                                <div>
+                                    <DeliveryTruck className="w-16"/>
+                                </div>
+                                <div className="text-lg font-medium">
+                                    Automatisez vos tournées et optimisez vos livraisons !
+                                </div>
+                            </div>
+                            <div className="border-t text-gray-500 dark:text-gray-400 pt-4 border-gray-200 dark:border-gray-700">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent
+                                libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
+                                imperdiet. Duis sagittis ipsum. Praesent mauris.
+                            </div>
+                            <div className="pt-4">
+                                <Button>
+                                    En savoir plus
+                                    <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                         xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                              clip-rule="evenodd"></path>
+                                    </svg>
+                                </Button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
