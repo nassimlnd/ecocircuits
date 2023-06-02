e49d494d9577ad4ae4bff103651f02de690aea6c
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-06-02 13:51:02 +0200

    Fix order invoice and add loading

--- src/pages/organisateur/orders/OrdersDetails.js
+++ src/pages/organisateur/orders/OrdersDetails.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import ConnectedLayout from "../../../layouts/ConnectedLayout";
 import {useParams} from "react-router-dom";
-import {Breadcrumb, Table} from "flowbite-react";
+import {Breadcrumb, Spinner, Table} from "flowbite-react";
 import logo from "../../../assets/logo.png";
 import OrdersService from "../../../services/OrdersService";
 import CustomersService from "../../../services/CustomersService";
@@ -11,6 +11,7 @@ import ProductsService from "../../../services/ProductsService";
 
 function OrdersDetails() {
     const params = useParams();
+    const [loading, setLoading] = useState(false);
     const [order, setOrder] = useState({});
     const [customer, setCustomer] = useState({});
     const [producers, setProducers] = useState([]);
@@ -18,6 +19,7 @@ function OrdersDetails() {
 
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);
             const responseDetails = await OrdersService.getOrdersDetails(params.id);
             setOrder(responseDetails.data);
             console.log(responseDetails.data);
@@ -26,6 +28,7 @@ function OrdersDetails() {
             setCustomer(responseCustomer.data);
 
             setProducts(responseDetails.data.orderProductDetails);
+            setLoading(false);
         }
         fetchData();
     }, []);
@@ -50,76 +53,91 @@ function OrdersDetails() {
                         </Breadcrumb.Item>
                     </Breadcrumb>
                 </div>
-                <div className="flex p-4 overflow-x-hidden">
-                    <div className="bg-white dark:bg-gray-800 md:w-1/2 shadow rounded-lg dark:text-white p-12">
-                        <div className="flex mb-8">
-                            <div className="w-1/2">
-                                <div className="text-xl font-bold">COMMANDE #{params.id}</div>
+                {!loading ? (
+                    <div className="md:flex m-4 overflow-x-hidden">
+                        <div className="bg-white dark:bg-gray-800 md:w-1/2 shadow rounded-lg dark:text-white p-8">
+                            <div className="md:flex mb-8 space-y-4 md:space-y-0">
+                                <div className="md:w-1/2">
+                                    <div className="text-xl font-bold">COMMANDE #{params.id}</div>
+                                </div>
+                                <div className="md:text-right md:w-1/2 md:space-y-0.5">
+                                    <div className="text-xl font-bold">EcoCircuits</div>
+                                    <div>1 rue de la Paix</div>
+                                    <div>75000 Paris</div>
+                                    <div>France</div>
+                                </div>
                             </div>
-                            <div className="text-right w-1/2 space-y-0.5">
-                                <div className="text-xl font-bold">EcoCircuits</div>
-                                <div>1 rue de la Paix</div>
-                                <div>75000 Paris</div>
-                                <div>France</div>
+                            <div className="font-bold text-xl">Facturé à</div>
+                            <div className="italic text-gray-500 dark:text-gray-400 mt-2">
+                                <div>{customer.nom} {customer.prenom}</div>
+                                <div>1 rue de la paix, 75000 PARIS</div>
+                                <div>{customer.email}</div>
+                                <div>{customer.telephone}</div>
+                            </div>
+
+                            <div className="relative overflow-x-auto sm:rounded-lg mt-12">
+                                <Table hoverable={true}>
+                                    <Table.Head>
+                                        <Table.HeadCell>
+                                            Produit
+                                        </Table.HeadCell>
+                                        <Table.HeadCell>
+                                            Quantité
+                                        </Table.HeadCell>
+                                        <Table.HeadCell>
+                                            Prix unitaire
+                                        </Table.HeadCell>
+                                        <Table.HeadCell>
+                                            Prix total
+                                        </Table.HeadCell>
+                                    </Table.Head>
+
+                                    <Table.Body className="divide-y">
+                                        {products.map((product) => (<>
+                                            <Table.Row
+                                                className="bg-white dark:bg-gray-800 dark:text-white font-semibold border-b border-gray-200 dark:border-gray-700"
+                                                key={product.idProduit}
+                                            >
+                                                <Table.Cell className="flex flex-col">
+                                                    {product.libelle}
+                                                </Table.Cell>
+                                                <div className="ml-8 mb-4">Fourni par :
+                                                    {product.orderProductProducers.map((producer) => (
+                                                        <div>
+                                                            {producer.libelle} | Quantité : {producer.quantite}
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                                <Table.Cell>
+                                                    {product.quantite}
+                                                </Table.Cell>
+                                                <Table.Cell>
+                                                    {product.prix ? product.prix : "-"} €
+                                                </Table.Cell>
+                                                <Table.Cell>
+                                                    {product.prix && product.quantite ? product.prix * product.quantite : "-"} €
+                                                </Table.Cell>
+                                            </Table.Row>
+                                        </>))}
+                                    </Table.Body>
+                                </Table>
                             </div>
                         </div>
-                        <div className="font-bold text-xl">Facturé à</div>
-                        <div className="italic text-gray-500 dark:text-gray-400 mt-2">
-                            <div>{customer.nom} {customer.prenom}</div>
-                            <div>1 rue de la paix, 75000 PARIS</div>
-                            <div>{customer.email}</div>
-                            <div>{customer.telephone}</div>
-                        </div>
-
-                        <div className="relative overflow-x-auto sm:rounded-lg mt-12">
-                            <Table hoverable={true}>
-                                <Table.Head>
-                                    <Table.HeadCell>
-                                        Produit
-                                    </Table.HeadCell>
-                                    <Table.HeadCell>
-                                        Quantité
-                                    </Table.HeadCell>
-                                    <Table.HeadCell>
-                                        Prix unitaire
-                                    </Table.HeadCell>
-                                    <Table.HeadCell>
-                                        Prix total
-                                    </Table.HeadCell>
-                                </Table.Head>
-
-                                <Table.Body className="divide-y">
-                                    {products.map((product) => (<>
-                                        <Table.Row
-                                            className="bg-white dark:bg-gray-800 dark:text-white font-semibold border-b border-gray-200 dark:border-gray-700"
-                                            key={product.idProduit}
-                                        >
-                                            <Table.Cell className="flex flex-col">
-                                                {product.libelle}
-                                            </Table.Cell>
-                                            <div className="ml-8 mb-4">Fourni par :
-                                                {product.orderProductProducers.map((producer) => (
-                                                    <div>
-                                                        {producer.libelle} | Quantité : {producer.quantite}
-                                                    </div>
-                                                ))}
-                                            </div>
-                                            <Table.Cell>
-                                                {product.quantite}
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {product.prix ? product.prix : "-"} €
-                                            </Table.Cell>
-                                            <Table.Cell>
-                                                {product.prix && product.quantite ? product.prix * product.quantite : "-"} €
-                                            </Table.Cell>
-                                        </Table.Row>
-                                    </>))}
-                                </Table.Body>
-                            </Table>
+                    </div>) : (
+                    <div className="h-noscroll">
+                        <div
+                            className="flex flex-col items-center justify-center h-full m-auto gap-y-6 text-center">
+                            <Spinner
+                                aria-label="Extra large spinner example"
+                                size="xl"
+                                className="text-center mb-4 mt-8"
+                            />
+                            <p className="text-lg mb-8 text-gray-500 dark:text-gray-300">
+                                Chargement...
+                            </p>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </ConnectedLayout>
     </>)
